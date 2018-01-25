0637303f5df0c9a4122690bcb19a48b3f88b6d8e
eki <eki>
2018-01-25 18:33:19 +0200

    s

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
 <link rel="stylesheet" type="text/css" href="/css/skillstack.css">
 </head>
+<body>
 <?php 
 echo "moro"; 
 echo "test";
@@ -12,4 +13,5 @@ echo "test";
 echo "moo";
 echo "j";
 ?>
+</body>
 </html>
