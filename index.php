b166b5bbc345feed89d0d320cbfba9d9a6e6bb06
eki <eki>
2018-01-25 20:02:03 +0200

    navbar

--- index.php
+++ index.php
@@ -8,25 +8,27 @@
 </head>
 <body>
 <nav class="navbar navbar-inverse bg-primary">
-<a class="navbar-brand" href="#">Navbar</a>
-
-<div class="collapse navbar-collapse" id="navbarSupportedContent">
+<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+<a class="navbar-brand" href="#">SkillStack.</a>
+<div class="collapse navbar-collapse">
   <ul class="navbar-nav mr-auto">
     <li class="nav-item active">
       <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="#">Link</a>
+      <a class="nav-link" href="#">Skills</a>
     </li>
     <li class="nav-item">
-      <a class="nav-link disabled" href="#">Disabled</a>
+      <a class="nav-link" href="#">Get in touch</a>
     </li>
   </ul>
   <form class="form-inline my-2 my-lg-0">
     <input class="form-control mr-sm-2" type="text" placeholder="Search">
     <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
   </form>
-</div>
+  </div>
 </nav>
 <i class="fa fa-cog fa-spin"></i>
 <?php 
