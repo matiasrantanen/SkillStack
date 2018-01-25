2c6397cac869a6ece02b6f1aa090662ec14d6b3c
eki <eki>
2018-01-25 18:30:41 +0200

    added css

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+<link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="/css/skillstack.css">
+</head>
 <?php 
 echo "moro"; 
 echo "test";
@@ -5,3 +12,4 @@ echo "test";
 echo "moo";
 echo "j";
 ?>
+</html>
