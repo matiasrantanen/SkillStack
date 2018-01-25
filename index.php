54f031b343c3518898893885ea8545ff09a13eaf
eki <eki>
2018-01-25 20:57:25 +0200

    JS working

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 <html lang="en">
 <head>
 <script defer src="js/fontawesome-all.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script defer src="js/bootstrap.min.js"></script>
 <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 <link rel="stylesheet" type="text/css" href="css/skillstack.css">
 </head>
