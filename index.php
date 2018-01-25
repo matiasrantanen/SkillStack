e452e831828cae3b72a65c0b8195d6b155ca3790
eki <eki>
2018-01-25 20:46:50 +0200

    nav2

--- index.php
+++ index.php
@@ -7,29 +7,31 @@
 <link rel="stylesheet" type="text/css" href="css/skillstack.css">
 </head>
 <body>
-<nav class="navbar navbar-inverse bg-primary">
-<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+<nav class="navbar navbar-toggleable-md navbar-inverse bg-primary">
+<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-<a class="navbar-brand" href="#">SkillStack.</a>
-<div class="collapse navbar-collapse">
-  <ul class="navbar-nav mr-auto">
-    <li class="nav-item active">
-      <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">Skills</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">Get in touch</a>
-    </li>
-  </ul>
-  <form class="form-inline my-2 my-lg-0">
-    <input class="form-control mr-sm-2" type="text" placeholder="Search">
-    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-  </form>
+  <a class="navbar-brand" href="#"><i class="fas fa-th-large"></i> SkillStack</a>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Profile<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Skills</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contact</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="text" placeholder="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
   </div>
 </nav>
+<div class="container">
 <i class="fa fa-cog fa-spin"></i>
 <?php 
 echo "moro"; 
@@ -38,5 +40,6 @@ echo "test";
 echo "moo";
 echo "j";
 ?>
+</div>
 </body>
 </html>
