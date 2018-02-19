19da32058dcef610e5c07016bd6b1d70dc56874e
eki <eerik.reis@gmail.com>
2018-02-19 21:47:22 +0200

    createprofile writing into table first test

--- createprofile.php
+++ createprofile.php
@@ -1,5 +1,50 @@
 <!DOCTYPE html>
+<?php
+//include config file for database connection
+require_once 'config.php';
 
+$name = $description = "";
+$name_err = $description_err = "";
+
+
+
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+  // Validate name
+  if(empty(trim($_POST["firstname"]))){
+      $name_err = "Please enter your name.";
+  }
+  if(empty(trim($_POST["aboutme"]))){
+    $description_err = "Tell us something about yourself!";
+}
+//get input from user and insert into table
+  $name = $_POST['firstname'];
+  $description = $_POST['aboutme'];
+  $sqli = "INSERT INTO profiles (name,description) VALUES ('$name','$description')";
+  if(mysqli_query($link, $sqli)){
+    echo "Table created successfully.";
+} else{
+    echo "ERROR: Could not able to execute $sqli. " . mysqli_error($link);
+}
+  /*creates table with $name to database when submitted
+
+  $name = $_POST['firstname'];
+    $sqli = "CREATE TABLE $name (
+      name VARCHAR(10) NOT NULL,
+      description VARCHAR(255) NOT NULL
+      )";
+      if(mysqli_query($link, $sql)){
+        echo "Table created successfully.";
+    } else{
+        echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+    }
+    */
+  
+  
+mysqli_close($link);
+}
+?>
 <html lang="en">
 <head>
 <style type="text/css">
@@ -30,21 +75,17 @@
         <h2>Time to create your SkillStack profile!</h2>
         <p>Please fill this form to create an account.</p>
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-            <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+            <div class="form-group <?php echo (!empty($name_err)) ? 'has-error' : ''; ?>">
                 <label>Your name:<sup>*</sup></label>
-                <input type="text" name="username"class="form-control" value="<?php echo $username; ?>">
-                <span class="help-block"><?php echo $username_err; ?></span>
+                <input type="text" name="firstname"class="form-control" value="<?php echo $name; ?>">
+                <span class="help-block"><?php echo $name_err; ?></span>
             </div>    
-            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+            <div class="form-group <?php echo (!empty($description_err)) ? 'has-error' : ''; ?>">
                 <label>Let others know what makes you special!<sup>*</sup></label>
-                <input type="password" name="password" class="form-control" value="<?php echo $password; ?>">
-                <span class="help-block"><?php echo $password_err; ?></span>
+                <input type="text" name="aboutme" class="form-control" value="<?php echo $description; ?>">
+                <span class="help-block"><?php echo $description_err; ?></span>
             </div>
-            <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
-                <label>Show off your coding skills!<sup>*</sup></label>
-                <input type="password" name="confirm_password" class="form-control" value="<?php echo $confirm_password; ?>">
-                <span class="help-block"><?php echo $confirm_password_err; ?></span>
-            </div>
+            
             <div class="form-group">
                 <input type="submit" class="btn btn-primary" value="Submit">
                 <input type="reset" class="btn btn-default" value="Reset">
