4d04adacbb39835fc8687097b2182dc02d448368
eki <eerik.reis@gmail.com>
2018-02-19 17:00:58 +0200

    createprofile page

---
+++ createprofile.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+<script defer src="js/fontawesome-all.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script defer src="js/bootstrap.min.js"></script>
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="css/skillstack.css">
+</head>
+<body>
+<nav class="navbar navbar-toggleable-md navbar-inverse bg-primary">
+<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <a class="navbar-brand" href="#"><i class="fas fa-th-large"></i> SkillStack</a>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      
+    </ul>
+    
+  </div>
+</nav>
+
+<footer class="footer">
+  <div class="container" id="foot">
+    <div class="row" id=copy>
+      <p id="copy"><i class="far fa-copyright"></i>SkillStack</p>
+</div>
+</div>
+</footer>
+    
+</body>
+</html>
